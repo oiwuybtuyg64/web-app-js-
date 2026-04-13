3f5962349fa95c89dae679adce7ceb45135c326f
oiwuybtuyg64 <morskojcygan36@gmail.com>
2026-04-13 14:54:37 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,8 @@
+const bids = [
+    {id: 1, title: "Иван Дмитриевич", value: 345,    status: "работает", createdAt: "2026-12-12"},
+    {id: 2, title: "Настасия Кириловна",   value: 127,     status: "работает", createdAt: "2027-03-02"},
+    {id: 3, title: "Иван Петрович",   value:890 ,    status: "new",  createdAt: "2020-04-04"},
+    {id: 4, title: "Ксения Михайловна",   value: 567,     status: "работает",  createdAt: "2024-03-05"},
+    {id: 5, title: "Илья Фёдорович", value:9345 ,    status: "работает",  createdAt: "2026-06-12"},
+    {id: 6, title: "Пётр Иванови",   value:801 ,     status: "new",  createdAt: "2026-05-20"},
+];
